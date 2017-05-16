f88919a5c49fe76034d91ed2c59558f52dc68e89
cesar <cesar@Megan>
2017-05-16 03:25:40 -0500

    formularios para los reportes añadidos...(1.1)

--- src/Vista/Reportes/formProductosxProveedor.java
+++ src/Vista/Reportes/formProductosxProveedor.java
@@ -9,12 +9,12 @@ package Vista.Reportes;
  *
  * @author cesar
  */
-public class formularioProductos extends javax.swing.JFrame {
+public class formProductosxProveedor extends javax.swing.JFrame {
 
     /**
      * Creates new form formProductos
      */
-    public formularioProductos() {
+    public formProductosxProveedor() {
         initComponents();
     }
 
@@ -34,7 +34,7 @@ public class formularioProductos extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jLabel1.setText("Reportes de Proveedores");
+        jLabel1.setText("Reportes de Productos por Proveedor");
 
         jLabel2.setText("ID Proveedor: ");
 
@@ -52,7 +52,7 @@ public class formularioProductos extends javax.swing.JFrame {
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addComponent(jLabel1))
-                .addContainerGap(145, Short.MAX_VALUE))
+                .addContainerGap(89, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(btnGenerarReporte)
@@ -92,21 +92,23 @@ public class formularioProductos extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(formularioProductos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(formProductosxProveedor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(formularioProductos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(formProductosxProveedor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(formularioProductos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(formProductosxProveedor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(formularioProductos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(formProductosxProveedor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new formularioProductos().setVisible(true);
+                new formProductosxProveedor().setVisible(true);
             }
         });
     }
